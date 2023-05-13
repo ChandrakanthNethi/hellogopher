1499947aa1f702b0ba010a06f2fd9fb92392f351
Chandrakanth Nethi <chandrakanth.nethi@gmail.com>
2023-05-13 14:03:50 +0530

    feat: hello world!

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/ChandrakanthNethi/stringUtils"
+)
+
+func main() {
+
+	result := stringUtils.Upper("hello world!")
+	println(result)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ChandrakanthNethi/hellogopher
+
+go 1.17
+
+require github.com/ChandrakanthNethi/stringUtils v0.0.0-20230513081900-e8e1de6457b4
